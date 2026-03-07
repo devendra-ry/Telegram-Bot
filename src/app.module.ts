844be8ebb1377d9abd987037e9eae498b6e5e9e4
devendra yadav <YadavDevendra@protonmail.com>
2026-03-07 22:50:52 +0530

    Fix Telegraf module config injection in Nest app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from "@nestjs/common";
 import { TelegrafModule } from "nestjs-telegraf";
 import { AppConfigService } from "./app-config.service.js";
+import { loadConfig } from "./config.js";
 import { GeminiService } from "./gemini.service.js";
 import { ConversationStateService } from "./conversation-state.service.js";
 import { TelegramUpdateHandler } from "./telegram.update.js";
 import { TelegramWebhookService } from "./telegram-webhook.service.js";
 
+const bootConfig = loadConfig();
+
 @Module({
   imports: [
-    TelegrafModule.forRootAsync({
-      inject: [AppConfigService],
-      useFactory: (appConfig: AppConfigService) => ({
-        token: appConfig.config.telegramToken,
-        launchOptions: false,
-      }),
+    TelegrafModule.forRoot({
+      token: bootConfig.telegramToken,
+      launchOptions: false,
     }),
   ],
   providers: [
